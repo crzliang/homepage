59de4b19a4697c9025255b9c32c29182f9b347a7
crzliang <lkzxgp@gmail.com>
2026-07-06 01:53:16 +0800

    feat: 添加busuanzi站点访问量统计脚标

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ export default function App() {
                     <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer" className="hover:text-slate-300 transition-colors">桂ICP备2022011217号</a>
                     <img src="https://qcloudimg.tencent-cloud.cn/raw/eed02831a0e201b8d794c8282c40cf2e.png" alt="" width="14" className="mx-1" />
                     <a href="https://beian.mps.gov.cn/#/query/webSearch?code=45032302000182" target="_blank" rel="noopener noreferrer" className="hover:text-slate-300 transition-colors">桂公网安备45032302000182号</a>
+                    <span id="busuanzi_container_site_pv">本站总访问量 <span id="busuanzi_value_site_pv"></span> 次</span>
                 </div>
             </footer>
         </div>
